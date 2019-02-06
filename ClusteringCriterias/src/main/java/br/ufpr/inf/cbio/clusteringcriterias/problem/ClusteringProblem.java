d62675b83fa518b6f19dbecf7dfaf49b378dd26f
Gian Fritsche <gian.fritsche@gmail.com>
2019-02-06 09:07:33 -0200

    Maven project and hello world test

---
+++ ClusteringCriterias/src/main/java/br/ufpr/inf/cbio/clusteringcriterias/problem/ClusteringProblem.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2019 Gian Fritsche <gmfritsche at inf.ufpr.br>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.ufpr.inf.cbio.clusteringcriterias.problem;
+
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+/**
+ *
+ * @author Gian Fritsche <gmfritsche at inf.ufpr.br>
+ */
+public class ClusteringProblem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! [" + JMetalRandom.getInstance().nextDouble() + "]");
+    }
+}
