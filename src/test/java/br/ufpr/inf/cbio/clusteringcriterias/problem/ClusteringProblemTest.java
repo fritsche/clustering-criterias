20a3bebbd479379ff84367682693e6c4d35d52cc
Gian Fritsche <gian.fritsche@gmail.com>
2019-02-14 15:51:17 -0200

    test evaluate Overall deviation and Connectivity

--- src/test/java/br/ufpr/inf/cbio/clusteringcriterias/problem/ClusteringProblemTest.java
+++ src/test/java/br/ufpr/inf/cbio/clusteringcriterias/problem/ClusteringProblemTest.java
@@ -17,8 +17,10 @@
 package br.ufpr.inf.cbio.clusteringcriterias.problem;
 
 import br.ufpr.inf.cbio.clusteringcriterias.criterias.ObjectiveFunction;
+import br.ufpr.inf.cbio.clusteringcriterias.criterias.impl.Connectivity;
 import br.ufpr.inf.cbio.clusteringcriterias.criterias.impl.OverallDeviation;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -89,6 +91,48 @@ public class ClusteringProblemTest {
         double actual = s.getObjective(0);
         System.out.println("Evaluate Clustering Problem using Overall Deviation, expected: " + expected + ", actual: " + actual);
         assertEquals(expected, actual, 0.0);
+    }
+
+    /**
+     * Test of evaluate method, of class ClusteringProblem.
+     */
+    @Test
+    public void testEvaluateOverallDeviationAndConnectivity() {
+        System.out.println("evaluateOverallDeviationAndConnectivity");
+
+        DataSet dataSet = new DataSet();
+
+        dataSet.addDataPoint("a", new ArrayPoint(new double[]{1.0, 2.0}));
+        dataSet.addDataPoint("b", new ArrayPoint(new double[]{-1.0, 2.0}));
+        dataSet.addDataPoint("c", new ArrayPoint(new double[]{1.0, -1.0}));
+        dataSet.addDataPoint("d", new ArrayPoint(new double[]{-1.0, -1.0}));
+
+        int maxK = 2;
+
+        List<ObjectiveFunction> functions = new ArrayList<>(1);
+        functions.add(new OverallDeviation(dataSet, new EuclideanDistance()));
+
+        List<List<Integer>> neighborhood = new ArrayList<>(4);
+        neighborhood.add(new ArrayList<>(Arrays.asList(1)));
+        neighborhood.add(new ArrayList<>(Arrays.asList(0)));
+        neighborhood.add(new ArrayList<>(Arrays.asList(3)));
+        neighborhood.add(new ArrayList<>(Arrays.asList(2)));
+        functions.add(new Connectivity(neighborhood));
+
+        ClusteringProblem problem = new ClusteringProblem(true, dataSet, functions, maxK);
+        IntegerSolution s = problem.createSolution();
+
+        s.setVariableValue(0, 0); // solution 'a' cluster 0
+        s.setVariableValue(1, 0); // solution 'b' cluster 0
+
+        s.setVariableValue(2, 1); // solution 'c' cluster 1
+        s.setVariableValue(3, 1); // solution 'd' cluster 1
+
+        problem.evaluate(s);
+
+        double[] expected = new double[]{4.0, 0.0};
+        double[] actual = s.getObjectives();
+        assertTrue(Arrays.equals(expected, actual));
     }
 
 }
